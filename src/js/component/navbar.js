98e09235eaf3d7947f242ff78caa4c240b231acb
alonsogomezo <luisalonsogo95@gmail.com>
2022-05-28 02:56:35 +0000

    hola

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,16 +2,38 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-light bg-light mb-3">
+      <Link to="/">
+        <span className="navbar-brand mb-0 h1">React Boilerplate</span>
+      </Link>
+      <div className="ml-auto">
+        <Link to="/demo">
+          <div className="dropdown">
+            <button
+              className="btn btn-secondary dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Dropdown button
+            </button>
+            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a className="dropdown-item" href="#">
+                Action
+              </a>
+              <a className="dropdown-item" href="#">
+                Another action
+              </a>
+              <a className="dropdown-item" href="#">
+                Something else here
+              </a>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </nav>
+  );
 };
---
+++ src/js/views/planetDetails.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function Planeta() {
+  return (
+    <div>
+        <div className='row'>
+            <div className='col-6'>
+                <img />
+            </div>
+            <div className='col-6'>
+                <h1>aa</h1>
+                <p>aa</p>
+            </div>
+        </div>
+        <div className='row border-top border-danger' >
+            <div className='col-2'>a</div>
+            <div className='col-2'>b</div>
+            <div className='col-2'>c</div>
+            <div className='col-2'>d</div>
+            <div className='col-2'>e</div>
+            <div className='col-2'>f</div>
+        </div>
+    </div>
+  )
+}
