4c4596ebec915cc2ff7f442b17e4674c54a6ad2c
karenvicent <karenvergara@msn.com>
2022-05-31 23:55:49 +0000

    primer avance

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,9 +1,10 @@
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { Navbar } from "../component/navbar";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		hola
+	<div>
+		karen
 	</div>
 );
